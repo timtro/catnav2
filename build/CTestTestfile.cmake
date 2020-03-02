e7ce6607f571cfedbff949573e087e76ec08944a
timtro <timtro@gmail.com>
2020-03-02 15:48:36 -0500

    Ignoring build files.

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/timtro/workspace/vme-ctrl-algebra
-# Build directory: /home/timtro/workspace/vme-ctrl-algebra/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
--- build/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/timtro/workspace/vme-ctrl-algebra/lib
-# Build directory: /home/timtro/workspace/vme-ctrl-algebra/build/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
