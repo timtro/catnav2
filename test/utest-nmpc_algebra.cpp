5d43e9ee6ccf23899972c228ab6611d6f75e7c89
timtro <timtro@gmail.com>
2020-03-08 11:24:06 -0400

    Added unit-box test for forecasting, refactored some type noise out.

--- test/utest-nmpc_algebra.cpp
+++ test/utest-nmpc_algebra.cpp
@@ -1,8 +1,12 @@
+#include <vector>
+
 #include <catch2/catch.hpp>
 
 #include "../include/nmpc_algebra.hpp"
 
 TEST_CASE("Forcasting the pose of a Nav2 robot starting at the origin…") {
+  /* Catch::StringMaker<double>::precision = 20; */
+
   constexpr std::size_t N = 5;
 
   NMPCState<N> cx;
@@ -16,7 +20,10 @@ TEST_CASE("Forcasting the pose of a Nav2 robot starting at the origin…") {
 
   REQUIRE(cx.x[0] == 0.f);
   REQUIRE(cx.y[0] == 0.f);
-  REQUIRE(cx.v[N - 2] == 0.f);
+  REQUIRE(cx.th[0] == 0.f);
+  REQUIRE(cx.Dx[0] == 0.f);
+  REQUIRE(cx.Dy[0] == 0.f);
+  REQUIRE(cx.Dth[0] == 0.f);
 
   SECTION(
       "With no steering and velocity, the robot should remain stationary "
@@ -26,16 +33,16 @@ TEST_CASE("Forcasting the pose of a Nav2 robot starting at the origin…") {
 
     auto result = dtl::compute_forecast(std::move(cx));
 
-    REQUIRE_THAT(std::vector<double>(result.x, result.x + N),
-                 Catch::Equals<double>({0.f, 0.f, 0.f, 0.f, 0.f}));
-    REQUIRE_THAT(std::vector<double>(result.y, result.y + N),
-                 Catch::Equals<double>({0.f, 0.f, 0.f, 0.f, 0.f}));
-    REQUIRE_THAT(std::vector<double>(result.th, result.th + N),
-                 Catch::Equals<double>({0.f, 0.f, 0.f, 0.f, 0.f}));
-    REQUIRE_THAT(std::vector<double>(result.Dx, result.Dx + N),
-                 Catch::Equals<double>({0.f, 0.f, 0.f, 0.f, 0.f}));
-    REQUIRE_THAT(std::vector<double>(result.Dy, result.Dy + N),
-                 Catch::Equals<double>({0.f, 0.f, 0.f, 0.f, 0.f}));
+    REQUIRE_THAT(std::vector(result.x, result.x + N),
+                 Catch::Equals<double>({0, 0, 0, 0, 0}));
+    REQUIRE_THAT(std::vector(result.y, result.y + N),
+                 Catch::Equals<double>({0, 0, 0, 0, 0}));
+    REQUIRE_THAT(std::vector(result.th, result.th + N),
+                 Catch::Equals<double>({0, 0, 0, 0, 0}));
+    REQUIRE_THAT(std::vector(result.Dx, result.Dx + N),
+                 Catch::Equals<double>({0, 0, 0, 0, 0}));
+    REQUIRE_THAT(std::vector(result.Dy, result.Dy + N),
+                 Catch::Equals<double>({0, 0, 0, 0, 0}));
   }
 
   SECTION(
@@ -49,15 +56,40 @@ TEST_CASE("Forcasting the pose of a Nav2 robot starting at the origin…") {
 
     auto result = dtl::compute_forecast(std::move(cx));
 
-    REQUIRE_THAT(std::vector<double>(result.x, result.x + N),
-                 Catch::Equals<double>({0.f, 1.f, 2.f, 3.f, 4.f}));
-    REQUIRE_THAT(std::vector<double>(result.y, result.y + N),
-                 Catch::Equals<double>({0.f, 0.f, 0.f, 0.f, 0.f}));
-    REQUIRE_THAT(std::vector<double>(result.th, result.th + N),
-                 Catch::Equals<double>({0.f, 0.f, 0.f, 0.f, 0.f}));
-    REQUIRE_THAT(std::vector<double>(result.Dx, result.Dx + N),
-                 Catch::Equals<double>({1.f, 1.f, 1.f, 1.f, 1.f}));
-    REQUIRE_THAT(std::vector<double>(result.Dy, result.Dy + N),
-                 Catch::Equals<double>({0.f, 0.f, 0.f, 0.f, 0.f}));
+    REQUIRE_THAT(std::vector(result.x, result.x + N),
+                 Catch::Equals<double>({0, 1, 2, 3, 4}));
+    REQUIRE_THAT(std::vector(result.y, result.y + N),
+                 Catch::Equals<double>({0, 0, 0, 0, 0}));
+    REQUIRE_THAT(std::vector(result.th, result.th + N),
+                 Catch::Equals<double>({0, 0, 0, 0, 0}));
+    REQUIRE_THAT(std::vector(result.Dx, result.Dx + N),
+                 Catch::Equals<double>({1, 1, 1, 1, 1}));
+    REQUIRE_THAT(std::vector(result.Dy, result.Dy + N),
+                 Catch::Equals<double>({0, 0, 0, 0, 0}));
+  }
+
+  SECTION(
+      "Drive in a square by controlling Dth. With v = 1, dt=1 and Dth = π/2, "
+      "this should drive the unit-box.") {
+    for (auto& each : cx.v) each = 1.f;
+    for (auto& each : cx.Dth) each = M_PI / 2;
+    for (auto& each : cx.th) each = 0.f;
+    cx.Dx[0] = 1.f;
+    cx.Dy[0] = 0.f;
+
+    auto result = dtl::compute_forecast(std::move(cx));
+
+    REQUIRE_THAT(std::vector(result.x, result.x + N),
+                 Catch::Approx<double>({0, 1, 1, 0, 0}).margin(1e-15));
+    REQUIRE_THAT(std::vector(result.y, result.y + N),
+                 Catch::Approx<double>({0, 0, 1, 1, 0}).margin(1e-15));
+    REQUIRE_THAT(
+        std::vector(result.th, result.th + N),
+        Catch::Approx<double>({0, M_PI / 2, M_PI, 3 * M_PI / 2, 2 * M_PI})
+            .margin(1e-15));
+    REQUIRE_THAT(std::vector(result.Dx, result.Dx + N),
+                 Catch::Approx<double>({1, 0, -1, 0, 1}).margin(1e-15));
+    REQUIRE_THAT(std::vector(result.Dy, result.Dy + N),
+                 Catch::Approx<double>({0, 1, 0, -1, 0}).margin(1e-15));
   }
 }
