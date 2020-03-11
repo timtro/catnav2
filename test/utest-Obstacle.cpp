95b0d52ce0f261a80b41e240a0e8db5b390bef42
timtro <timtro@gmail.com>
2020-03-10 23:04:05 -0400

    Evolved unit tests.

--- test/utest-Obstacle.cpp
+++ test/utest-Obstacle.cpp
@@ -1,28 +1,48 @@
+#define CATCH_CONFIG_ENABLE_PAIR_STRINGMAKER
+
+#include <limits>
 #include <vector>
 
+#include <boost/hana/functional/curry.hpp>
 #include <catch2/catch.hpp>
 
 #include "../lib/Obstacle.hpp"
 
-template <typename T, typename U>
-std::pair<T, U> operator+(const std::pair<T, U>& l, const std::pair<T, U>& r) {
-  return {l.first + r.first, l.second + r.second};
-}
+using boost::hana::curry;
 
-template <typename T, typename U>
-std::pair<T, U> operator+=(const std::pair<T, U>& l, const std::pair<T, U>& r) {
-  return {l.first + r.first, l.second + r.second};
-}
+using XY = std::pair<double, double>;
 
-TEST_CASE("Null obstacles produce no potential") {
+TEST_CASE("Null obstacles produce no potential", "[NullObstacle]") {
   std::vector<Obstacle> obs{NullObstacle(), NullObstacle()};
-  std::pair<double, double> total = {0, 0};
 
-  double x = 0;
-  double y = 0;
-
-  for (auto& ob : obs) total += g_phi(x, y, ob);
-
-  REQUIRE(total == std::pair<double, double>{0, 0});
+  REQUIRE(g_phi(0, 0, NullObstacle()) == XY{0, 0});
+  REQUIRE(g_phi(100, 100, NullObstacle()) == XY{0, 0});
 }
 
+TEST_CASE("Testing point obstacle at origin with hand-computed values",
+          "[PointObstacle]") {
+  const auto o1 = PointObstacle{0, 0, 2, 1};
+  const double big = std::numeric_limits<double>::max() / 2;
+
+  REQUIRE(g_phi(0, 0, o1) == XY{0, 0});
+  REQUIRE(g_phi(1, 0, o1) == XY{0.5, 0});
+  REQUIRE(g_phi(0, 1, o1) == XY{0, 0.5});
+  REQUIRE(g_phi(1, -1, o1) == XY{2. / 9, -2. / 9});
+  REQUIRE(g_phi(-10, 100, o1) == XY{-20. / 102030201, 200. / 102030201});
+  // Vanish at infinity:
+  REQUIRE(g_phi(0, big, o1) == XY{0, 0});
+  REQUIRE(g_phi(big, 0, o1) == XY{0, 0});
+
+  const auto o2 = PointObstacle{0, 0, 1, 1};
+
+  REQUIRE((std::isnan(g_phi(0, 0, o2).first)
+           && std::isnan(g_phi(0, 0, o2).second)));
+  REQUIRE(g_phi(1, 0, o2) == XY{1. / 4, 0});
+  REQUIRE(g_phi(0, 1, o2) == XY{0, 1. / 4});
+  REQUIRE(g_phi(1, -1, o2)
+          == XY{1 / (M_SQRT2 * (M_SQRT2 + 1) * (M_SQRT2 + 1)),
+                -1 / (M_SQRT2 * (M_SQRT2 + 1) * (M_SQRT2 + 1))});
+  // Vanish at infinity:
+  REQUIRE(g_phi(0, big, o2) == XY{0, 0});
+  REQUIRE(g_phi(big, 0, o2) == XY{0, 0});
+}
