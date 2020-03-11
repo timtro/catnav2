66629094242c9356ef9c667a184dcc346979a786
timtro <timtro@gmail.com>
2020-03-11 12:22:20 -0400

    Expanded tests to off-origin, refactored to reduce noise in test lines with λ-wrapper.

--- test/utest-Obstacle.cpp
+++ test/utest-Obstacle.cpp
@@ -12,6 +12,8 @@ using boost::hana::curry;
 
 using XY = std::pair<double, double>;
 
+const double big = std::numeric_limits<double>::max() / 2;
+
 TEST_CASE("Null obstacles produce no potential", "[NullObstacle]") {
   std::vector<Obstacle> obs{NullObstacle(), NullObstacle()};
 
@@ -19,30 +21,55 @@ TEST_CASE("Null obstacles produce no potential", "[NullObstacle]") {
   REQUIRE(g_phi(100, 100, NullObstacle()) == XY{0, 0});
 }
 
-TEST_CASE("Testing point obstacle at origin with hand-computed values",
+TEST_CASE("Testing point obstacles at origin with hand-computed values",
           "[PointObstacle]") {
-  const auto o1 = PointObstacle{0, 0, 2, 1};
-  const double big = std::numeric_limits<double>::max() / 2;
+  constexpr auto o1 = PointObstacle{0, 0, 2, 1};
+  constexpr auto g_o1 = [o1](double x, double y) { return g_phi(x, y, o1); };
 
-  REQUIRE(g_phi(0, 0, o1) == XY{0, 0});
-  REQUIRE(g_phi(1, 0, o1) == XY{0.5, 0});
-  REQUIRE(g_phi(0, 1, o1) == XY{0, 0.5});
-  REQUIRE(g_phi(1, -1, o1) == XY{2. / 9, -2. / 9});
-  REQUIRE(g_phi(-10, 100, o1) == XY{-20. / 102030201, 200. / 102030201});
+  REQUIRE(g_o1(0, 0) == XY{0, 0});
+  REQUIRE(g_o1(1, 0) == XY{0.5, 0});
+  REQUIRE(g_o1(0, 1) == XY{0, 0.5});
+  REQUIRE(g_o1(1, -1) == XY{2. / 9, -2. / 9});
+  REQUIRE(g_o1(-10, 100) == XY{-20. / 102030201, 200. / 102030201});
   // Vanish at infinity:
-  REQUIRE(g_phi(0, big, o1) == XY{0, 0});
-  REQUIRE(g_phi(big, 0, o1) == XY{0, 0});
+  REQUIRE(g_o1(0, big) == XY{0, 0});
+  REQUIRE(g_o1(big, 0) == XY{0, 0});
 
-  const auto o2 = PointObstacle{0, 0, 1, 1};
+  constexpr auto o2 = PointObstacle{0, 0, 1, 1};
+  constexpr auto g_o2 = [o2](double x, double y) { return g_phi(x, y, o2); };
 
-  REQUIRE((std::isnan(g_phi(0, 0, o2).first)
-           && std::isnan(g_phi(0, 0, o2).second)));
-  REQUIRE(g_phi(1, 0, o2) == XY{1. / 4, 0});
-  REQUIRE(g_phi(0, 1, o2) == XY{0, 1. / 4});
-  REQUIRE(g_phi(1, -1, o2)
+  // By quirk of the math, with exponent 1, g_phi is undef at the origin.
+  REQUIRE((std::isnan(g_o2(0, 0).first) && std::isnan(g_o2(0, 0).second)));
+
+  REQUIRE(g_o2(1, 0) == XY{1. / 4, 0});
+  REQUIRE(g_o2(0, 1) == XY{0, 1. / 4});
+  REQUIRE(g_o2(1, -1)
           == XY{1 / (M_SQRT2 * (M_SQRT2 + 1) * (M_SQRT2 + 1)),
                 -1 / (M_SQRT2 * (M_SQRT2 + 1) * (M_SQRT2 + 1))});
   // Vanish at infinity:
-  REQUIRE(g_phi(0, big, o2) == XY{0, 0});
-  REQUIRE(g_phi(big, 0, o2) == XY{0, 0});
+  REQUIRE(g_o2(0, big) == XY{0, 0});
+  REQUIRE(g_o2(big, 0) == XY{0, 0});
+
+  constexpr auto o3 = PointObstacle{0, 0, 2, 2};
+  constexpr auto g_o3 = [o3](double x, double y) { return g_phi(x, y, o3); };
+
+  REQUIRE(g_o3(0, 0) == XY{0, 0});
+  REQUIRE(g_o3(1, 0) == XY{2. / 9, 0});
+  REQUIRE(g_o3(0, 1) == XY{0, 2. / 9});
+  REQUIRE(g_o3(1, -1) == XY{1. / 8, -1. / 8});
+  // Vanish at infinity:
+  REQUIRE(g_o3(0, big) == XY{0, 0});
+  REQUIRE(g_o3(big, 0) == XY{0, 0});
+}
+
+TEST_CASE("Testing point obstacles off-origin", "[PointObstacle]") {
+
+  constexpr auto o1 = PointObstacle{1, 1, 2, 1};
+  constexpr auto g_o1 = [o1](double x, double y) { return g_phi(x, y, o1); };
+
+  REQUIRE(g_o1(1, 1) == XY{0, 0});
+  REQUIRE(g_o1(2, 1) == XY{0.5, 0});
+  REQUIRE(g_o1(1, 2) == XY{0, 0.5});
+  REQUIRE(g_o1(2, 0) == XY{2. / 9, -2. / 9});
+  REQUIRE(g_o1(-9, 101) == XY{-20. / 102030201, 200. / 102030201});
 }
